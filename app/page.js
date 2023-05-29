d3deae60c712450a544fe0c870d8d666c50acc21
JordanUA <rudenkosasha40@gmail.com>
2023-05-29 17:37:57 +0300

    last_update

--- app/page.js
+++ app/page.js
@@ -1,92 +1,76 @@
 import Image from 'next/image'
+
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <header className={styles.header}>
+      <div className={styles.home_page}>
+        <nav id="site-nav" className={styles.nav_bar}>
+          <ul className={styles.menu_primary}>
+
+            <li id="menu-item-home">
+              <a title="home" href="#site-nav">Home</a>
+            </li>
+            <li id="menu-item-about">
+              <a title="about" href="#about">About</a>
+            </li>
+            <li id="menu-item-projects">
+              <a title="projects" href="#projects">Projects</a>
+            </li>
+            <li id="menu-item-contact">
+              <a title="contact" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className={styles.name_avatar}>
+
+        <h1>Rudenko Oleksandr</h1>
+        <h2>Front-end developer</h2>
+        <p>I don`t know what to write here)))</p>
+      </div>
+      <div className={styles.avatar}><img src="https://images.unsplash.com/photo-1522069169874-c58ec4b76be5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=712&q=80"></img></div>
+      <section className={styles.main}>
+        <div className={styles.about_me}>
+          <h2 id="about">ABOUT</h2>
+          <div className={styles.text_about_me}>
+            <p>I'm an 18-year-old student at a very cool DUICT university who likes front-end and web development. This is my first portfolio, so I don't know what to write here)))</p>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <div className={styles.projects}>
+          <h2 id="projects">PROJECTS</h2>
+          <p>My projects from the university</p>
+        </div>
+        <div className={styles.photo_projects}>
+          <p><a href="https://github.com/JordanUA/frontbackjs">JS</a></p>
+          <p>---------</p>
+          <p><a href="https://github.com/JordanUA/frontlaba">Frontlaba</a></p>
+        </div>
+        <div className={styles.contacts}>
+          <h2 id="contact">Contact</h2>
+          <p></p>
+        </div>
+        <div className={styles.network}>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            My number: +380666246274
           </p>
-        </a>
-      </div>
-    </main>
+          <p>
+            My telegram: @shyrik2474
+          </p><p>
+            My instagram: shyrik2474
+          </p>
+        </div>
+      </section>
+
+
+    </header>
+
+
+
+
+
+
+
   )
 }
